af97666afecb45bbe1fb78890e0fcc0a50ffa563
Railson25 <railsonsantiago123@gmail.com>
2020-03-01 14:04:13 -0300

    Acabamento dos Botões

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,34 @@
 import React from 'react'
 import './Button.css'
 
-export default props => 
-<button className='button'>{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    console.log(classes)
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}   
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}  
+  //onClick retorna o mesmo  conteudo do botao ussando
+        // && para garantir que so vai entrar na segunda parte
+        //da expressão se a primeira for válida 
+
+//soluçao 02 usando jsx
+//export default props => 
+//definindo uma expressão sendo delimitada pela crazy
+//sempre que tem uma expressçao e chaves dentro de um jsx
+// eu posso colocar código JavaScript
+//    <button className={`
+//        button
+//        ${props.operation ? 'operation' : ''}
+//        ${props.double ? 'double' : ''}
+//        ${props.triple ? 'triple' : ''}
+//    `}>
+//        {props.label}
+//    </button>
